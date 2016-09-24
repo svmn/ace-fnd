1ae5a4bc6855a46193e9fcbf422476180d5724d2
nyaa <root@tuzach.in>
2016-09-24 19:08:29 +0300

    online counter

--- src/app/containers/header.js
+++ src/app/containers/header.js
@@ -6,11 +6,12 @@ import Menu from '../components/menu';
 
 class Header extends Component {
   render() {
+    const { topic, online } = this.props;
     return (
       <div className='top header'>
         <Menu />
         <div className='topic'>Tuzach development version 0.0.1</div>
-        <div className='online'>Онлайн: 12</div>
+        <div className='online'>Онлайн: {online}</div>
       </div>
     );
   }
