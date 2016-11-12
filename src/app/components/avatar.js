608ba45e7290c102dc019f9061277d3078bf306c
nyaa <root@tuzach.in>
2016-11-12 15:12:19 +0200

    avatar tooltip

--- src/app/components/avatar.js
+++ src/app/components/avatar.js
@@ -6,24 +6,54 @@ import FontIcon from 'material-ui/FontIcon';
 import { getAvatarColor } from '../utils';
 
 class UserAvatar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showTooltip: false
+    };
+    this.showTooltip = this.showTooltip.bind(this);
+    this.hideTooltip = this.hideTooltip.bind(this);
+  }
+
+  showTooltip() {
+    this.setState({ showTooltip: true });
+  }
+
+  hideTooltip() {
+    this.setState({ showTooltip: false });
+  }
+
   render() {
     const { userId } = this.props;
     const color = getAvatarColor(userId);
     return (
-      <Avatar
-        backgroundColor={color}
-        size={20}
-        color='rgba(255,255,255,0.8)'
-        icon={<FontIcon className='fa fa-user' />}
-        style={{
-          cursor: 'pointer',
-          // android < 4.4 fixes
-          display: 'block',
-          textAlign: 'center',
-          lineHeight: '20px'
-        }}
-        title={userId}
-      />
+      <div>
+        <Avatar
+          backgroundColor={color}
+          size={20}
+          color='rgba(255,255,255,0.8)'
+          icon={<FontIcon className='fa fa-fw fa-user' />}
+          style={{
+            cursor: 'pointer'
+          }}
+          onMouseEnter={this.showTooltip}
+          onMouseLeave={this.hideTooltip}
+        />
+        <div
+          style={{
+            display: this.state.showTooltip ? 'block' : 'none',
+            position: 'absolute',
+            top: '10px',
+            left: '20px',
+            color: 'white',
+            backgroundColor: 'rgba(0,0,0,0.7)',
+            padding: '0 8px',
+            zIndex: '3000'
+          }}
+        >
+          {userId}
+        </div>
+      </div>
     );
   }
 }
