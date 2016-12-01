903c50e787c243f9551c30c552c298896c822954
nyaa <root@tuzach.in>
2016-12-01 22:54:26 +0200

    fix playlist upload button on ff

--- src/app/components/playlistUploadButton.js
+++ src/app/components/playlistUploadButton.js
@@ -9,49 +9,55 @@ import { fullWhite } from 'material-ui/styles/colors';
 export default function PlaylistUploadButton(props) {
   const { uploadProgress, upload } = props;
   return (
-    <FloatingActionButton
+    <div
       style={{
         position: 'absolute',
         right: '16px',
         bottom: '16px'
       }}
-      mini
     >
-      <div>
-        {
-          !uploadProgress ? null : (
-            <CircularProgress
-              color={fullWhite}
-              mode='determinate'
-              value={uploadProgress}
-            />
-          )
-        }
-        <FontIcon
-          className='material-icons'
-          color={fullWhite}
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            margin: '8px 0 0 8px'
-          }}
-        >playlist_add</FontIcon>
+      <FloatingActionButton mini>
+        <div>
+          {
+            // div above for Uncaught TypeError: Cannot read property 'style' of undefined
+            !uploadProgress ? null : (
+              <CircularProgress
+                color={fullWhite}
+                mode='determinate'
+                value={uploadProgress}
+              />
+            )
+          }
+          <FontIcon
+            className='material-icons'
+            color={fullWhite}
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              margin: '8px 0 0 8px'
+            }}
+          >playlist_add</FontIcon>
+        </div>
+      </FloatingActionButton>
+      <div
+        style={{
+          position: 'absolute',
+          top: '0',
+          right: '0',
+          width: '40px',
+          height: '40px',
+          opacity: 0,
+          cursor: 'pointer',
+          overflow: 'hidden'
+        }}
+      >
         <input
           type='file'
           onChange={e => upload(e.target.files[0])}
-          style={{
-            position: 'absolute',
-            top: '0',
-            right: '0',
-            width: '40px',
-            height: '40px',
-            opacity: 0,
-            cursor: 'pointer'
-          }}
         />
       </div>
-    </FloatingActionButton>
+    </div>
   );
 }
 
